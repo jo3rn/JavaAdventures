1011eb946d68ac13148eaaa14921061020eb9d72
Joern Auerbach <joern.auerbach@informatik.hs-fulda.de>
2020-10-23 08:53:54 +0200

    Ask player for name

---
+++ JavaAdventures.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class JavaAdventures {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String playerName;
+		int playerChoice;
+
+		System.out.println("You stranded on an island.");
+		System.out.println("What is your name?");
+		scanner.nextLine();
+	}
+}
